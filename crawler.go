e032ec58a08e6f16cb3a347b190a19fad2890e29
Johannes Kroß <johanneskross@gmail.com>
2013-05-02 13:51:20 +0200

    fmt

--- crawler.go
+++ crawler.go
@@ -14,22 +14,26 @@ var domains = []string{"domain1", "domain2", "domain3", "domain4", "domain5"}
 func pullUrl(domain string, url string, c chan bool) {
 	fmt.Printf("started %s\n", domain)
 
+	// pull url
 	resp, err := http.Get(url)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 
-	fmt.Printf("finished %s\n", domain)
+	// signal that pull is done
 	c <- true
+
+	fmt.Printf("finished %s\n", domain)
+
 }
 
 func startDomains() {
-	// start domains and pull url on each domain
+	// make channel for go routines to signal their end
 	c := make(chan bool, len(domains))
+
+	// pull url on each domain
 	for _, v := range domains {
 		go pullUrl(v, URL, c)
 	}
