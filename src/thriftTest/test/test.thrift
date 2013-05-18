6961269acc16b0b17fc69944315f9b49fe6d9a5c
Johannes Kroß <johanneskross@gmail.com>
2013-05-18 11:49:46 +0200

    deleted unnecessary test files

--- src/thriftTest/test/test.thrift
+++
@@ -1,8 +0,0 @@
-      struct UserProfile {
-        1: i32 uid,
-        2: string name,
-        3: string blurb
-      }
-      service UserStorage {
-        bool store(1: UserProfile user),
-      }
